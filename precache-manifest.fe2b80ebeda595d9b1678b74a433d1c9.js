9c0ace37fdc1f9acef5234585b7cb76dad8a7e95
Sam Devaprasad <unclesam@umich.edu>
2019-03-13 22:13:25 -0400

    Updates

---
+++ precache-manifest.fe2b80ebeda595d9b1678b74a433d1c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cream-spot/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "e841c2d5c0b0d59c8e5e",
+    "url": "/cream-spot/static/js/runtime~main.fc7f82cb.js"
+  },
+  {
+    "revision": "026ea67ffa4688ecaa09",
+    "url": "/cream-spot/static/js/main.ff8326d9.chunk.js"
+  },
+  {
+    "revision": "ba264cd90c4d36546d5a",
+    "url": "/cream-spot/static/js/2.8f71a45e.chunk.js"
+  },
+  {
+    "revision": "026ea67ffa4688ecaa09",
+    "url": "/cream-spot/static/css/main.c4aff73a.chunk.css"
+  },
+  {
+    "revision": "f140db6fde5148b0c87e1f231150a68f",
+    "url": "/cream-spot/index.html"
+  }
+];
